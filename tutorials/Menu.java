55a5a28efca6be82ab05e521e4bb08adfc1424bc
Istiaq Abdullah <abdullahistiaq77@gmail.com>
2026-03-15 15:26:41 +0000

    Add Menu class with initial implementation

---
+++ tutorials/Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+    public static void main(String[] args){
+        System.out.println("Welcome to the restaurant!");
+        System.out.println("********");
+        System.out.println("* MENU *");
+        System.out.println("********");
+        System.out.println("1. Pizza");
+        System.out.println("2. Burger");
+        System.out.println("3. Salad");
+
+
+    }    
+}
