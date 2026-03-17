94137285019e870265102e6325d2532bac822382
Istiaq Abdullah <abdullahistiaq77@gmail.com>
2026-03-17 19:26:28 +0000

    Add Factorial class to calculate the factorial of a user-input number

---
+++ tutorials/Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+        int factorial = 1;
+
+        for (int i=number; i>=1; i--) {
+            factorial = factorial * 1;
+        } System.out.println("Factorial: " +factorial);
+    }
+    
+}
