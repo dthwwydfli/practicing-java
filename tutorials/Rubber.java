8b09fee6e1c3aa3d2412ba48076bac1054483fc7
Istiaq Abdullah <abdullahistiaq77@gmail.com>
2026-03-18 17:27:54 +0000

    Add Rubber class to display a countdown from 9 to 1

---
+++ tutorials/Rubber.java
@@ -0,0 +1,10 @@
+public class Rubber {
+    public static void main(String[] args) {
+        for (int i = 9; i > 0; i--) {
+            for (int j = 9; j > 0; j--) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
