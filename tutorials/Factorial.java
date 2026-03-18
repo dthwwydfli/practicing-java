f26d815442d0d568acdddad245bf9a9c43d90b79
Istiaq Abdullah <abdullahistiaq77@gmail.com>
2026-03-18 17:41:14 +0000

    Error fixed

--- tutorials/Factorial.java
+++ tutorials/Factorial.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number : ");
+        System.out.println("Enter a number: ");
         int number = input.nextInt();
         int factorial = 1;
 
