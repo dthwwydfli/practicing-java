c12939ef120fec92d205428fd26a9551e9d17e45
Istiaq Abdullah <abdullahistiaq77@gmail.com>
2026-03-25 11:52:36 +0000

    Add Methods class with a simple method and main execution

---
+++ tutorials/Methods.java
@@ -0,0 +1,9 @@
+public class Methods {
+    private static void my_method(){
+        System.out.println("This is my first method!");
+    }
+
+    public static void main (String[] args){
+        my_method();
+    }
+}
